91c8cd8dd2b3d00927a9e15325a221269762a848
sergei <s@kontrast.by>
2017-03-03 11:05:08 +0300

    gfljkl

---
+++ src/by/it/SergeiSor/jd01_05/Main.java
@@ -0,0 +1,11 @@
+package by.it.SergeiSor.jd01_05;
+
+
+public class Main {
+    public static void main(String[] args){
+        // Задача 1
+        double a=1.21;
+        double b=0.371;
+        System.out.println(Runner1.optionL(y));
+        }
+}
---
+++ src/by/it/SergeiSor/jd01_05/Runner1.java
@@ -0,0 +1,21 @@
+package by.it.SergeiSor.jd01_05;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+import static java.lang.Math.asin;
+import static java.lang.Math.tan;
+import static java.lang.Math.cbrt;
+
+public class Runner1 {
+    public static double optionL(double y){
+        double a= (1,21 + 0,371)*2;
+        double b=;
+        double c=;
+        double d=;
+
+        y= a-(b+c)-d;
+      return y;
+
+
+    }
+}
